446cd11faf3b0def6f850b127e763645730933fb
ElectroPerf <kunmun.devroms@gmail.com>
2021-03-08 07:21:44 +0000

    dotOS Official

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The dotOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01BD.mk
+    $(LOCAL_DIR)/dot_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-userdebug
+    dot_X01BD-userdebug
---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+export SELINUX_IGNORE_NEVERALLOWS=true
+export SKIP_ABI_CHECKS=true
+rm -rf vendor/qcom/opensource/power
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_power.git -b lineage-18.1 vendor/qcom/opensource/power
+rm -rf hardware/qcom-caf/msm8998
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/display
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
+rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr.git -b lineage-18.1 vendor/qcom/opensource/data-ipa-cfg-mgr
+git clone https://gitlab.com/ElectroPerf/vendor_addons_oneplus_apps.git -b OOS11 vendor/addons/oneplus/apps
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
+rm -rf packages/resources/devicesettings
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-18.1 packages/resources/devicesettings
+git clone https://github.com/ElectroPerf/vendor_extras.git -b 11 vendor/extras
