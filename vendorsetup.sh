0bf8012c0ed78f1b9199adc93395e00b0c115ddf
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-03-08 10:34:01 +0000

    Track packages/resources/devicesettings from lineage

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,3 +11,5 @@ git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-c
 git clone https://gitlab.com/ElectroPerf/vendor_addons_oneplus_apps.git -b OOS11 vendor/addons/oneplus/apps
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
 git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
+rm -rf packages/resources/devicesettings
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-18.1 packages/resources/devicesettings
